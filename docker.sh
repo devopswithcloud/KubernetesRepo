5cdd926a3a2e605b35e12071349648d9f5de6d8b
devopswithcloud <65442863+devopswithcloud@users.noreply.github.com>
2022-05-21 21:05:04 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce=5:19.03.12~3-0~ubuntu-bionic -y
+sudo apt-mark hold docker-ce
+sudo docker version
